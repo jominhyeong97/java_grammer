48acaa3dbfdb19ee6f7b15492fc6006111735350
jominhyeong97 <jumbo0303@daum.net>
2025-06-16 16:29:56 +0900

    6월 16일

--- src/C01Basic/C10Set.java
+++ src/C01Basic/C10Set.java
@@ -7,64 +7,66 @@ import java.util.TreeSet;
 
 public class C10Set {
     public static void main(String[] args) {
-//        중복이 없고 순서가 보장안됨.
+////        중복이 없고 순서가 보장안됨.
+//
+//        Set<String> mySet = new HashSet<>();
+//        mySet.add("5");
+//        mySet.add("5");
+//        mySet.add("5");
+//        mySet.add("5");
+//        mySet.add("5");
+//        mySet.add("3");
+//        mySet.add("1");
+//        mySet.add("3");
+//        mySet.add("5");
+//        System.out.println(mySet);
+//
+//
+////        백준 : 숫자카드(10815)
+//
+//
+////        프로그래머스 : 폰켓몬
+//
+//
+//
+////        LinkedHashSet : 입력순서 보장
+//        Set<Integer> linkedSet = new LinkedHashSet<>();
+//        linkedSet.add(1);
+//        linkedSet.add(1);
+//        linkedSet.add(0);
+//        linkedSet.add(3);
+//        linkedSet.add(1);
+//        System.out.println(linkedSet);  //103
+//
+//
+////        TreeSet : 데이터를 오름차순 정렬
+//        Set<Integer> treeSet = new TreeSet<>();
+//        treeSet.add(1);
+//        treeSet.add(1);
+//        treeSet.add(0);
+//        treeSet.add(3);
+//        treeSet.add(1);
+//        System.out.println(treeSet);  //013
+//
+//
+//
+////       집합관련함수 : 교집합(retainAll), 합집합(addAll), 차집합(removeAll)
+//        Set<String> set1 = new HashSet<>();
+//        set1.add("java");
+//        set1.add("python");
+//        set1.add("C++");
+//        Set<String> set2 = new HashSet<>();
+//        set2.add("java");
+//        set2.add("html");
+//        set2.add("css");
 
-        Set<String> mySet = new HashSet<>();
-        mySet.add("5");
-        mySet.add("5");
-        mySet.add("5");
-        mySet.add("5");
-        mySet.add("5");
-        mySet.add("3");
-        mySet.add("1");
-        mySet.add("3");
-        mySet.add("5");
-        System.out.println(mySet);
-
-
-//        백준 : 숫자카드(10815)
-
-
-//        프로그래머스 : 폰켓몬
-
-
-
-//        LinkedHashSet : 입력순서 보장
-        Set<Integer> linkedSet = new LinkedHashSet<>();
-        linkedSet.add(1);
-        linkedSet.add(1);
-        linkedSet.add(0);
-        linkedSet.add(3);
-        linkedSet.add(1);
-        System.out.println(linkedSet);  //103
-
-
-//        TreeSet : 데이터를 오름차순 정렬
-        Set<Integer> treeSet = new TreeSet<>();
-        treeSet.add(1);
-        treeSet.add(1);
-        treeSet.add(0);
-        treeSet.add(3);
-        treeSet.add(1);
-        System.out.println(treeSet);  //013
-
-
-
-//       집합관련함수 : 교집합(retainAll), 합집합(addAll), 차집합(removeAll)
-        Set<String> set1 = new HashSet<>();
-        set1.add("java");
-        set1.add("python");
-        set1.add("C++");
-        Set<String> set2 = new HashSet<>();
-        set2.add("java");
-        set2.add("html");
-        set2.add("css");
 //        set1.retainAll(set2); //set1에는 java만 남음
 //        System.out.println(set1);
 //        set1.addAll(set2); //java phyon c++ html css
 //        System.out.println(set1);
 //        set1.removeAll(set2); //python c++
 //        System.out.println(set1);
+
 
 
 
